c718c90b4fd22f0bff89f1d60cdfe43a677f634a
Ragib Ali <66816874+ragibalikhan@users.noreply.github.com>
2026-01-25 13:12:01 +0530

    Update project and client satisfaction statistics

--- client/src/components/EnhancedAbout.tsx
+++ client/src/components/EnhancedAbout.tsx
@@ -5,10 +5,10 @@ import { ScrollReveal, ScrollCounter, MagneticElement, TypewriterText } from "./
 import { FaRocket, FaUsers, FaAward, FaGlobe, FaCode, FaBrain, FaHeart, FaStar } from "react-icons/fa";
 
 const stats = [
-  { number: 150, suffix: "+", title: "Projects Delivered", icon: FaRocket },
-  { number: 98, suffix: "%", title: "Client Satisfaction", icon: FaHeart },
+  { number: 500, suffix: "+", title: "Projects Delivered", icon: FaRocket },
+  { number: 300, suffix: "%", title: "Client Satisfaction", icon: FaHeart },
   { number: 25, suffix: "+", title: "Team Members", icon: FaUsers },
-  { number: 12, suffix: "+", title: "Countries Served", icon: FaGlobe }
+  { number: 10, suffix: "+", title: "Countries Served", icon: FaGlobe }
 ];
 
 const values = [
